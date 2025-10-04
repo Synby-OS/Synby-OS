0ad882963eb0fdfabe43e8b3310c9819bee8bcf3
Synby <git@syntheenby.simplelogin.com>
2025-10-04 01:05:52 -0500

    fix cachy-kerneh.sh and protonvpn.sh
    run hwe-modules.yml before common-modyles.yml

--- files/scripts/cachy-kernel.sh
+++ files/scripts/cachy-kernel.sh
@@ -7,9 +7,9 @@ setsebool -P domain_kernel_load_modules on
 
 dnf5 versionlock delete kernel{,-core,-modules,-modules-core,-modules-extra,-tools,-tools-lib,-headers,-devel,-devel-matched}
 
-dnf5 copr enable bieszczaders/kernel-cachyos
+dnf5 -y copr enable bieszczaders/kernel-cachyos
 
-dnf5 install kernel-cachyos kernel-cachyos-devel-matched
+dnf5 -y install --allowerasing kernel-cachyos kernel-cachyos-devel-matched
 
 dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 
--- files/scripts/protonvpn.sh
+++ files/scripts/protonvpn.sh
@@ -6,7 +6,7 @@ set -exuo pipefail
 wget "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.3-1.noarch.rpm"
 
 # install rpm housing repofile and refresh repos
-dnf5 install ./protonvpn-stable-release-1.0.3-1.noarch.rpm && dnf5 check-update --refresh
+dnf install -y ./protonvpn-stable-release-1.0.3-1.noarch.rpm && dnf5 check-update --refresh
 
 # install proton vpn
-dnf5 install proton-vpn-gnome-desktop
+dnf install -y proton-vpn-gnome-desktop
