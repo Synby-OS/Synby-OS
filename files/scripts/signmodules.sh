ced3918d4bd9ef077841fc4d273109bc7d998a02
Synby <107513274+Synby@users.noreply.github.com>
2025-10-04 23:03:55 -0500

    Update signmodules.sh

--- files/scripts/signmodules.sh
+++ files/scripts/signmodules.sh
@@ -21,7 +21,7 @@ if [ -z "$MODULE_NAME" ]; then
   exit 1
 fi
 
-KERNEL_VERSION="$(rpm -q "kernel" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+KERNEL_VERSION="$(rpm -q "kernel-cachyos" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
 PUBLIC_KEY_DER_PATH="../system/etc/pki/akmods/certs/akmods-synby-os.der"
 PUBLIC_KEY_CRT_PATH="./certs/public_key.crt"
